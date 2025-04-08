dd689610b40c42309b136df314bdfa4c32b99eb6
HarishVijendiran97 <harish.vijendiran@gmail.com>
2025-04-08 23:26:28 +0530

    add: frontend url to dotenv

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://joblistingadmin.vercel.app",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: "*",
 };
